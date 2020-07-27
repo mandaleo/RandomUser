1554e0cce095f0a22de83278f2d53ce6610816b6
Manuel Martinez Gomez <manu.martinez.gomez@gmail.com>
2020-07-27 19:43:04 +0200

    Add searchbar in navigationview

--- RandomUser/RandomUser/Features/ListUsers/View/ListUsersViewController.swift
+++ RandomUser/RandomUser/Features/ListUsers/View/ListUsersViewController.swift
@@ -5,10 +5,22 @@ final class ListUsersViewController: UIViewController {
   var presenter: ListUsersPresenter?
   var listUsersView: ListUsersView?
   
+  private var searchBarView: UISearchBar = {
+    let searchBar = UISearchBar()
+    searchBar.searchBarStyle = .prominent
+    searchBar.placeholder = " Search..."
+    searchBar.sizeToFit()
+    searchBar.isTranslucent = false
+    searchBar.showsCancelButton = true
+    return searchBar
+  }()
+  
   override func loadView() {
     super.loadView()
     view = listUsersView as? UIView
     listUsersView?.delegate = self
+    navigationItem.titleView = searchBarView
+    searchBarView.delegate = self
   }
   
   override func viewDidLoad() {
@@ -30,5 +42,31 @@ extension ListUsersViewController: ListUsersUI {
 extension ListUsersViewController: ListsUsersViewDelegate {
   func didSelect(user: User) {
     presenter?.didSelect(user: user)
+  }
+}
+
+// MARK: - UISearchBarDelegate
+extension ListUsersViewController: UISearchBarDelegate {
+  func searchBar(_ searchBar: UISearchBar, textDidChange textSearched: String) {
+    NSObject.cancelPreviousPerformRequests(withTarget: self,
+                                           selector: #selector(filterList),
+                                           object: nil)
+    perform(#selector(filterList), with: nil, afterDelay: 0.5)
+  }
+  
+  @objc private func filterList() {
+    print("filtrar")
+  }
+  
+  func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+    searchBar.resignFirstResponder()
+  }
+  
+  func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+    searchBar.resignFirstResponder()
+  }
+  
+  private func dismissKeyboard() {
+    self.view.endEditing(true)
   }
 }
